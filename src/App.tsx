651f9bbc4ba3487f445b9f2dd1c98277587fdd84
yavuztokus <yavuztokus2@gmail.com>
2025-02-16 12:00:15 +0000

    adding a footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Pricing from "./components/Pricing";
 import Header from "./components/Header";
 import ThemeWrapper from "./components/ThemeWrapper";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <ThemeWrapper>
       <Header />
       <Pricing />
+      <Footer />
     </ThemeWrapper>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-100 dark:bg-gray-900 text-black dark:text-white p-4 text-center">
+      <p>&copy; 2025 Pricing Page</p>
+      <p>
+        Created by{" "}
+        <a
+          href="https://www.linkedin.com/in/yavuztokus/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-violet-500 dark:text-violet-300"
+        >
+          Yavuz Tokus
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
